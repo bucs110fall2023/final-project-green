c1618074753ac8a33fe1b9629d402345e2034b9e
ehom1 <erichom888@gmail.com>
2023-11-15 19:56:43 -0500

    mlm

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -6,16 +6,36 @@ class Controller:
   def __init__(self):
     pygame.init()
     self.screen = pygame.display.set_mode()
-    self.width, self.height = pygame.display.get_window_size()
+    self.width, self.height = pygame.display.get_window_size(())
     self.background = pygame.Surface((self.width, self.height))
     self.background.fill("lightblue")
     
-    doodle = Character((self.width/2, self.height/2))
-    
+    self.doodles = pygame.sprite.Group()
+    doodle = Character((self.width/2), (self.height/2))
+    self.doodles.add(doodle)
+        
     pygame.display.flip()
     
-  # def mainloop(self):
+  def mainloop(self):
   #   #select state loop
+      running = True
+      clock = pygame.time.Clock()
+
+      while running:
+          for event in pygame.event.get():
+              if event.type == pygame.QUIT:
+                  running = False
+
+          self.doodles.update()
+
+          self.screen.blit(self.background, (0, 0))
+          self.doodles.draw(self.screen)
+
+          pygame.display.flip()
+          clock.tick(60)
+
+      pygame.quit()
+
     
   
   # ### below are some sample loop states ###
