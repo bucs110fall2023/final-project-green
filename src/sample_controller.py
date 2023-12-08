efd54f0e49fb25b37ede6b2e5cfc142f09e3cbf2
ehom1 <erichom888@gmail.com>
2023-12-07 19:07:56 -0500

    j

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -11,6 +11,7 @@ class Controller:
         self.width, self.height = pygame.display.get_window_size()
         self.background = pygame.Surface((self.width, self.height))
         self.background.fill("lightblue")
+        pygame.display.set_caption("CS 110 Final Project - Doodle Jump")
 
         self.doodle = Character((self.width/2), (self.height - 60))
         
@@ -24,7 +25,7 @@ class Controller:
         self.gravity = 1
         self.max_plats = 10
         
-        self.font = pygame.font.Font(None, 55)
+        self.font = pygame.font.SysFont("bebas neue", 55)
         
         self.clock = pygame.time.Clock()
         self.fps = 60
@@ -56,11 +57,6 @@ class Controller:
                 self.doodle.rect.x -= 10
             elif(move_right):
                 self.doodle.rect.x += 10
-            
-            # plat_width = random.randint(40, 60)
-            # plat_x = random.randint(0, self.width - plat_width)
-            # plat_y = self.platform_group.sprites()[-1].rect.y - random.randint(80, 120)
-            # platform = Platforms(plat_x, plat_y, plat_width)
 
             if game_over == False:
                 if len(self.platform_group) < self.max_plats:
@@ -103,7 +99,7 @@ class Controller:
                 
                 self.clock.tick(self.fps)
             else:
-                game_over_text = self.font.render("Game Over!", True, "black")
+                game_over_text = self.font.render("Game Over!", True, "dimgray")
                 self.screen.blit(game_over_text, (150, 400))
                 
                 pygame.display.flip()
